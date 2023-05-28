713df5f98da9104b4f24886a250d1925739a0f94
unknown <wawerufelix60@gmail.com>
2023-05-28 21:57:56 +0300

    feat: added api call on debug

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import {Axios} from 'axios';
 import { CodeFundiPanel } from './CodeFundiPanel';
 import { SidebarProvider } from './SidebarProvider';
-
-const fundiV1 = 'https://code-fundi-api-git-api-felixwaweru.vercel.app';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -35,27 +32,21 @@ export function activate(context: vscode.ExtensionContext) {
 			// The code you place here will be executed every time your command is executed
 			const editor = vscode.window.activeTextEditor;
 			const selection = editor?.selection;
+
+			// If there's no code window open
+			if(!editor) {
+				vscode.window.showInformationMessage(`Active text editor not detected`);
+				return;
+			}
+
 			if (selection && !selection.isEmpty) {
 				const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
 				const highlighted = editor.document.getText(selectionRange);
 
-				const response = new Axios({
-					method: 'POST',
-					url: `${fundiV1}/v1/fundi/debug`,
-					data: {
-						api_key: "",
-						code_block: highlighted
-					},
-					headers: {
-						"Content-Type": "application/json",
-					},
-					responseType: 'stream'
+				sidebarProvider._view?.webview.postMessage({
+					type: 'debug',
+					value: highlighted
 				});
-
-				console.log(JSON.stringify(response));
-
-				// Display a message box to the user
-				vscode.window.showInformationMessage(`Code Fundi: ${highlighted}`);
 			}
 		})
 	);
--- webviews/components/CodeFundi.svelte
+++ webviews/components/CodeFundi.svelte
@@ -1,20 +1,101 @@
 <script>
+  import { onMount } from "svelte";
+  import axios from 'axios';
+
   let messages = [];
   let newMessage = "";
+  const fundiV1 = 'https://code-fundi-api.vercel.app';
 
-  function sendMessage() {
+  function debug(){
+      tsvscode.postMessage({
+        type: 'debug',
+        value: newMessage
+      });
+  }
+
+  function fundiDebug() {
     if (newMessage.trim() !== "") {
       messages = [...messages, newMessage];
       newMessage = "";
     }
+    // var message = '';
+
+    // if (event === undefined){
+    //   if (newMessage.trim() !== ""){
+    //     message = {'type': 'debug', 'value': newMessage};
+    //     console.log(message)
+    //   }
+    // }
+    // message = event.data; // The json data that the extension sent
+    // console.log(message)
+
+    // switch (message.type) {
+    //   case 'debug':
+    //     messages = [...messages, `<span style="color:#808080; font-weight: bold;">Debug:</span> <br> ${message.value}`];
+
+    //     // Loading message
+    //     messages = [...messages, '<span style="color:#808080; font-weight: bold;">Thinking 🧠...</span>'];
+
+    //     const response = axios({
+    //       method: 'POST',
+    //       url: `${fundiV1}/v1/fundi/debug`,
+    //       data: {
+    //         api_key: "",
+    //         code_block: message.value
+    //       },
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //       responseType: 'stream'
+    //     })
+    //     .then(response => {
+    //       messages.pop();
+    //       messages = [...messages, JSON.stringify(response.data)];
+    //     })
+    //     .catch(error => {
+    //       // Handle any errors
+    //       console.error(error);
+    //     });
+    //     break;
+    // }
   }
 
-  function callFundi() {
-    tsvscode.postMessage({
-      type: "onInfo",
-      value: "Info message"
+  onMount(() => {
+    window.addEventListener('message', event => {
+      const message = event.data; // The json data that the extension sent
+      console.log(message)
+
+      switch (message.type) {
+        case 'debug':
+          messages = [...messages, `<span style="color:#808080; font-weight: bold;">Debug:</span> <br> ${message.value}`];
+
+          // Loading message
+          messages = [...messages, '<span style="color:#808080; font-weight: bold;">Thinking 🧠...</span>'];
+
+          const response = axios({
+            method: 'POST',
+            url: `${fundiV1}/v1/fundi/debug`,
+            data: {
+              api_key: "",
+              code_block: message.value
+            },
+            headers: {
+              "Content-Type": "application/json",
+            },
+            responseType: 'stream'
+          })
+          .then(response => {
+            messages.pop();
+            messages = [...messages, JSON.stringify(response.data)];
+          })
+          .catch(error => {
+            // Handle any errors
+            console.error(error);
+          });
+          break;
+      }
     });
-  }
+  });
 </script>
 
 <style>
@@ -23,6 +104,22 @@
     flex-direction: column;
     height: 100%;
     padding: 16px;
+  }
+
+  .banner {
+    margin-bottom: 8px;
+    padding: 8px;
+    color: #ffffff;
+		text-align: center;
+    font-weight: bold;
+    font-size: x-large;
+  }
+
+  .welcome {
+    margin-bottom: 8px;
+    padding: 8px;
+    color: #ffffff;
+		text-align: center;
   }
 
   .message {
@@ -66,14 +163,22 @@
 </style>
 
 <div class="chat">
-  {#each messages as message}
-    <div class="message">{message}</div>
-  {/each}
+  {#if messages.length === 0}
+    <div class="banner">{@html "Code Fundi" }</div>
+    <br><br>
+    <div class="welcome">{@html 
+      "To get started, type in the message box below or highlight your code then right click to access the options."
+    }</div>   
+  {:else}
+    {#each messages as message}
+      <div class="message">{@html message}</div>
+    {/each}
+  {/if}
 
   <div class="message-box">
     <form on:submit|preventDefault={() => newMessage = ''}>
       <input type="text" class="message-input" bind:value={newMessage} />
-      <button class="send-button" on:click={callFundi}>Send a message</button>
+      <button class="send-button" on:click={fundiDebug}>Send a message</button>
     </form>
   </div>
 </div>
